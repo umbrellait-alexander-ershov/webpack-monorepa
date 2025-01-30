d935c16a93e3f862e1675fe42a46d64412f3f893
Aleksandr Ershov <alexander.ershov@umbrellait.com>
2025-01-30 16:29:15 +0300

    added storybook and added swc

---
+++ packages/shared/src/ui-kit/user-item/stories/user-item.stories.ts
@@ -0,0 +1,11 @@
+import { Meta } from "@storybook/react";
+import { UserItem } from "../user-item";
+
+const meta: Meta<typeof UserItem> = {
+  title: "Components/UserItem",
+  component: UserItem,
+};
+
+export default meta;
+
+export const page = {};
